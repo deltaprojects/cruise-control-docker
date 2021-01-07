067599fa86fff13254431bb65aa0f2d8c8ff973f
Marco Antonio Jonack <marcojck@users.noreply.github.com>
2021-01-07 13:48:46 -0300

    Fix CCFE 0.3.1+ latest release download (#1)

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 if [ ! -f /target/cruise-control-${VERSION}/kafka-cruise-control-start.sh ]; then
-  apk add --no-cache --virtual=.build-dependencies git curl ca-certificates && \
-  curl -JL https://github.com/linkedin/cruise-control/archive/${VERSION}.tar.gz | tar xzvf - -C /tmp && \
+  apk add --no-cache --virtual=.build-dependencies git curl wget ca-certificates && \
+  curl -JLs https://github.com/linkedin/cruise-control/archive/${VERSION}.tar.gz | tar xzvf - -C /tmp && \
   cd /tmp/cruise-control-${VERSION} && \
   git config --global user.email root@localhost && git config --global user.name root && \
   git init && git add . && git commit -m "Init local repo." && git tag -a ${VERSION} -m "Init local version." && \
@@ -15,8 +15,10 @@ if [ ! -f /target/cruise-control-${VERSION}/kafka-cruise-control-start.sh ]; the
   cp -a cruise-control-core/build/libs /target/cruise-control-${VERSION}/cruise-control-core/build && \
   cp -a config /target/cruise-control-${VERSION} && \
   cp -a kafka-cruise-control-start.sh /target/cruise-control-${VERSION} && \
-  curl -JL https://github.com/linkedin/cruise-control-ui/releases/latest/download/cruise-control-ui.tar.gz |
-  tar xzvf - -C /target --strip-components 1 --exclude '*/README.txt' && \
+  (printf "https://github.com" && \
+  (curl -sL https://github.com/linkedin/cruise-control-ui/releases/latest | \
+  grep "download.*cruise-control-ui-.*tar.gz" | cut -d '"' -f 2)) | \
+  wget -q -i - -O - | tar xzvf - -C /target --strip-components 1 && \
   mv /target/dist /target/cruise-control-ui && \
   cd && apk del .build-dependencies && rm -rf /tmp/cruise-control-${VERSION}
 fi
